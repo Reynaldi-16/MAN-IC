a6fdb34004e940d3ea278460f3fc4b62846e81bc
Reynaldi-16 <reynaldioktavian16@gmail.com>
2025-03-18 15:06:06 +0700

    [INDEX]awal

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+// console.log("halo Gailang");
+// document.write("saya senang sekali")
+
+let namaAja = "Reyy"
+console.log(namaAja);
+nama = "Gailang"
+// console.log(nama);
+AngkaAja = 1234
+// console.log(AngkaAja)
+let isfemale = false;
+console.log(typeof(isfemale))
+
+// let isfemale = false;
+
+
+// var nama1 
+// const nama2
